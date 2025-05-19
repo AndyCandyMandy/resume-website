3b46aca00181fe4b39bb9111e916f442c7e9966b
AndyMandyCandy <andyanan124@gmail.com>
2025-05-19 17:59:16 -0600

    Added place holder content to the homepoage
    - profile section lost its page responsiveness, need to go back and fix

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -9,26 +9,46 @@ function App() {
         <main className="homeBody"> 
 
 
-          <div className="profileBody">
+          <section className="profileBody">
             <h1>Who is <span style={{ color: "#ffc400" }}>Andy Giang</span>?</h1>
-            <picture>
+            <div className="profileContent">
+              <img src="/logo512.png" alt="Logo" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+              </p>
+            </div>
+          </section>
 
-            </picture>
-
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
-            </p>
-          </div>
-
-          <div className="projectBody">
+          <section className="projectBody">
             <h1>Projects</h1>
-          
-          </div> 
 
-          <div className="contactBody">
+            <div className="projectContent">
+              <h3>Project #1</h3>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+              </p>
+            </div>
+
+            <div className="projectContent">
+              <h3>Project #2</h3>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+              </p>
+            </div> 
+
+            <div className="projectContent">
+              <h3>Project #3</h3>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+              </p>
+            </div>
+          
+          </section> 
+
+          <section className="contactBody">
             <h1>Contact Me</h1>
 
-          </div>
+          </section>
 
 
         </main>
