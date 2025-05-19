c91ff589e8641163dbc3af2f21f5b1e160f82873
AndyMandyCandy <andyanan124@gmail.com>
2025-05-18 21:01:41 -0600

    Further developed the home page
    - Visually developed the header for the home page
    - Began an outline for the homepage

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -2,20 +2,34 @@ function App() {
   return ( 
     <div> 
         <header className="headerSection"> 
-            <p>Profile</p> 
-            <p>Projects</p> 
-            <p>Contacts</p>
-
+            <p className="headerBtn">Profile</p> 
+            <p className="headerBtn">Projects</p> 
+            <p className="headerBtn">Contacts</p>
         </header>
-        <main className="landingBody"> 
+        <main className="homeBody"> 
 
-          <div className="landingHeader"> 
-            <p>Greetings, I am <span style={{ color: "#ffc400" }}>Andy Giang</span>.</p> 
-            <p>Welcome to my website!</p>
-            <button className="landingBtn">Resume</button> 
 
-            
+          <div className="profileBody">
+            <h1>Who is <span style={{ color: "#ffc400" }}>Andy Giang</span>?</h1>
+            <picture>
+
+            </picture>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+            </p>
           </div>
+
+          <div className="projectBody">
+            <h1>Projects</h1>
+          
+          </div> 
+
+          <div className="contactBody">
+            <h1>Contact Me</h1>
+
+          </div>
+
 
         </main>
     </div>
