243f2880303f86edd35960aacd3b680d4b7b2e8a
AndyMandyCandy <andyanan124@gmail.com>
2025-05-19 19:08:57 -0600

    Fixed responsiveness of profile section in home page

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -12,7 +12,7 @@ function App() {
           <section className="profileBody">
             <h1>Who is <span style={{ color: "#ffc400" }}>Andy Giang</span>?</h1>
             <div className="profileContent">
-              <img src="/logo512.png" alt="Logo" />
+              <img src="/logo512.png" alt="Logo" style={{ maxWidth: "100%", height: "auto" }}/>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
               </p>
@@ -51,7 +51,10 @@ function App() {
           </section>
 
 
-        </main>
+        </main> 
+        <footer className="footSection">
+
+        </footer>
     </div>
   );
 }
