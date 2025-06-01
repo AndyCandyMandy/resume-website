9b0f4cdf1c79afb6adabe7bfd2296e6ba1191407
AndyMandyCandy <andyanan124@gmail.com>
2025-06-01 00:55:24 -0600

    Filled out 2 out of the 3 project sections
    - Added css to project buttons
    - Added warp trials preview image

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,6 +1,7 @@
 import { useInView } from "react-intersection-observer";
 
-import wizcatPreview from '../images/WizCat_ProjectPreview.png';
+import wizcatPreview from '../images/WizCat_ProjectPreview.png'; 
+import warptrialPreview from '../images/WarpTrial_ProjectPreview.png';
 
 function App() { 
   
@@ -87,13 +88,13 @@ function App() {
             <div className={`projectContent hiddenElement ${projectInView ? "fadeIn" : ""}`} style={{ animationDelay: "0.2s" }}>
               <div className="projectResBox">
                 <h3 className="projectHeader">WizCat Adventures</h3> 
-
+                <p className="projectRoleDescription">Game Developer, Design Lead, and Artist</p>
                 <p className="projectDescription"> 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+                  WizCat Adventures is an indie game project developed in the span of 2.5 months, utilizing the Unity Game Engine while showcasing proper elements of game development. With heavy inspiration from Noita, Wii Tanks, and Vampire Survivors, WizCat Adventures embraces the roguelike genre of its role models. The game was recognized as being the top project in the course due to our focus on well-rounded game mechanics, a polished, visually engaging interface, and our team collaboration within an agile environment.
                 </p> 
                 
                 <div className="projectBtnBox"> 
-                  <button className="projectBtn">About</button>  
+                  <a className="projectBtn" href="https://github.com/cmpt306team12/CMPT306-WizCat" target="_blank" rel="noreferrer">About</a> 
                   <a className="projectBtn" href="https://allahgold.itch.io/wizcat" target="_blank" rel="noreferrer">Project</a>
                 </div>
 
@@ -104,26 +105,25 @@ function App() {
 
             <div className={`projectContent hiddenElement ${projectInView ? "fadeIn" : ""}`} style={{ animationDelay: "0.4s" }}>
               <div className="projectResBox">
-                <h3 className="projectHeader">WizCat Adventures</h3> 
-
+                <h3 className="projectHeader">Warp Trials Research Program</h3> 
+                <p className="projectRoleDescription">Software Developer</p>
                 <p className="projectDescription"> 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
+                  A collaborative research project designed with the intention to gauge, compare, and record different cursor warping mechanisms based on entirely Java and its libraries. The program is organized through a model-view-controller (MVC) architecture for organization and scalability, aiming for a low-coupling and high-cohesion framework. Using the Fitts’s Index of Difficulty in conjunction with time-based trials, the program can collect and organize specific data, such as trial numbers, click errors, elapsed time, etc., to visually present in graph form.
                 </p> 
                 
                 <div className="projectBtnBox"> 
-                  <button className="projectBtn">About</button>  
-                  <a className="projectBtn" href="https://allahgold.itch.io/wizcat" target="_blank" rel="noreferrer">Project</a>
+                  <a className="projectBtn" href="https://github.com/AndyCandyMandy/warp-trials-mouse-to-target-research-program" target="_blank" rel="noreferrer">About</a>
                 </div>
 
               </div>
 
-              <img className="projectImage" src={wizcatPreview} alt="WizCat Preview"/> 
+              <img className="projectImage" src={warptrialPreview} alt="WarpTrial Preview"/> 
             </div> 
 
             <div className={`projectContent hiddenElement ${projectInView ? "fadeIn" : ""}`} style={{ animationDelay: "0.6s" }}>
               <div className="projectResBox">
-                <h3 className="projectHeader">WizCat Adventures</h3> 
-
+                <h3 className="projectHeader">Project #3</h3> 
+                <p className="projectRoleDescription">Fullstack Developer</p>
                 <p className="projectDescription"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam gravida aliquam leo quis porta. Sed elementum dolor ac lorem scelerisque, vel vulputate dui consequat. Pellentesque ut enim id tellus luctus gravida eget a tellus. Morbi hendrerit consequat velit, at scelerisque erat sodales sit amet. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nibh enim, mollis id sem a, viverra mollis nisi. Ut porttitor erat ac magna fringilla luctus.
                 </p> 
@@ -157,7 +157,7 @@ function App() {
         </main> 
         <footer className="footerSection"> 
           <a className="footerBtn devicon-github-original" href="https://github.com/AndyCandyMandy" target="_blank" rel="noreferrer"></a>
-          <a className="footerBtn devicon-linkedin-plain"></a>
+          <a className="footerBtn devicon-linkedin-plain" href="https://github.com/AndyCandyMandy" target="_blank" rel="noreferrer"></a>
 
         </footer>
     </div>
